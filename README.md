b8ed5abdb6b4ed9af3f69ca454918fc10769abff
Anton Kostiuchkov <anton.kostiuchkov@collibra.com>
2020-09-29 03:49:29 +0200

    pull down to refresh

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 - Chakra
 - Airtable
 - netlify + netlify functions
+- PWA
+- pull down to refresh
 
 - on slide down -> reload
--- src/App.js
+++ src/App.js
@@ -6,6 +6,46 @@ import { Product } from "./components/Product";
 import { Typography } from "./components/Typography";
 import { GlobalStyles } from "./components/GlobalStyles";
 
+var pStart = { x: 0, y: 0 };
+var pStop = { x: 0, y: 0 };
+
+function swipeStart(e) {
+  if (typeof e["targetTouches"] !== "undefined") {
+    var touch = e.targetTouches[0];
+    pStart.x = touch.screenX;
+    pStart.y = touch.screenY;
+  } else {
+    pStart.x = e.screenX;
+    pStart.y = e.screenY;
+  }
+}
+
+function swipeEnd(e) {
+  if (typeof e["changedTouches"] !== "undefined") {
+    var touch = e.changedTouches[0];
+    pStop.x = touch.screenX;
+    pStop.y = touch.screenY;
+  } else {
+    pStop.x = e.screenX;
+    pStop.y = e.screenY;
+  }
+
+  swipeCheck();
+}
+
+function swipeCheck() {
+  var changeY = pStart.y - pStop.y;
+  var changeX = pStart.x - pStop.x;
+  if (isPullDown(changeY, changeX)) {
+    window.location.reload();
+  }
+}
+
+function isPullDown(dY, dX) {
+  // methods of checking slope, length, direction of line created by swipe action
+  return dY < 0 && ((Math.abs(dX) <= 100 && Math.abs(dY) >= 200) || (Math.abs(dX) / Math.abs(dY) <= 0.3 && dY >= 60));
+}
+
 const getRecords = async () => {
   const url = `../.netlify/functions/airtable`;
   const { data } = await axios.get(url);
@@ -35,7 +75,7 @@ const transformProducts = (products) => {
 
 function App() {
   const [categories, setCategories] = useState({});
-  const [isEmpty, setIsEmpty] = useState();
+  // const [isEmpty, setIsEmpty] = useState();
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -44,13 +84,28 @@ function App() {
     };
 
     fetchProducts();
+
+    document.addEventListener(
+      "touchstart",
+      function (e) {
+        swipeStart(e);
+      },
+      false
+    );
+    document.addEventListener(
+      "touchend",
+      function (e) {
+        swipeEnd(e);
+      },
+      false
+    );
   }, []);
 
-  useEffect(() => {
-    const activeLists = Object.values(categories).filter((products) => products.length);
+  // useEffect(() => {
+  //   const activeLists = Object.values(categories).filter((products) => products.length);
 
-    setIsEmpty(!activeLists.length);
-  }, [categories]);
+  //   setIsEmpty(!activeLists.length);
+  // }, [categories]);
 
   const handleClick = ({ category, name, id }) => {
     const res = categories[category].filter((cat) => cat.name !== name);
@@ -66,11 +121,11 @@ function App() {
       <GlobalStyles />
       <Typography />
       <Box bg="gray.800">
-        {isEmpty && (
+        {/* {isEmpty && (
           <Box padding="4rem" color="gray.500" fontSize="2xl" textAlign="center">
             All grabbed. Good job!
           </Box>
-        )}
+        )} */}
 
         {Object.entries(categories).map(([category, products]) => {
           if (!products.length) {
